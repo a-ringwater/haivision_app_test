af58440292edcea9690454d35a0dfaac1c12e448
LordXGhost <adrien.anodeau@gmail.com>
2023-10-02 21:39:40 +0200

    Add export to next config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: "export",
+  basePath: "/haivision_app_test",
+  assetPrefix: "/haivision_app_test/",
   images: {
     domains: ['rickandmortyapi.com'],
   },
